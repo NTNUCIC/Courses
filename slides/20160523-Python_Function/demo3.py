12cc2cfa0165f1d6e7cc1cc94d9093d7c7b17686
Helen Liao <helenhelens89227@gmail.com>
2016-05-23 15:40:58 +0800

    add return

---
+++ slides/20160523-Python_Function/demo3.py
@@ -0,0 +1,24 @@
+def function1():
+	print('no return')
+
+def function2():
+	print('no return')
+	return
+
+def function3():
+	print('return bool')
+	return True;
+
+def function4():
+	print('return tuple')
+	return (1,2)
+
+def function5():
+	print('return tuple')
+	return 1,2
+
+function1()
+function2()
+function3()
+function4()
+function5()
