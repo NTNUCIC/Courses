65e93e137996350f6ee20124f72af5e6c3f95ff7
aigecko <aigecko@hotmail.com>
2016-12-01 19:52:07 +0800

    adapt to ruby-sdl-ffi

--- slides/20161202-RB-SDL-OOXX/SDL_OX/main.rb
+++ slides/20161202-RB-SDL-OOXX/SDL_OX/main.rb
@@ -2,25 +2,25 @@
 #coding: utf-8
 
 #一定要引入SDL函式庫
-require 'sdl'
+require 'ruby-sdl-ffi'
 
 #初始化SDL的繪圖功能
-SDL.init(SDL::INIT_VIDEO)
+SDL.Init(SDL::INIT_VIDEO)
 
 #取得螢幕300x300
-$Screen=SDL::Screen.open(300,300,32,SDL::SWSURFACE)
+$Screen=SDL.SetVideoMode(300,300,32,SDL::SWSURFACE)
 
 #設定先手方
 $side=:X
-SDL::WM.set_caption("現在輪到#{$side}下","")
+SDL.WM_SetCaption("現在輪到#{$side}下","")
 
 #載入棋子圖檔
 $Chess={
-  O: SDL::Surface.load("O.bmp"),
-  X: SDL::Surface.load("X.bmp")
+  O: SDL.LoadBMP("O.bmp"),
+  X: SDL.LoadBMP("X.bmp")
 }
 #以圖形左上角像素設定透明背景
-$Chess.each_value{|pic| pic.set_color_key(SDL::SRCCOLORKEY,pic[0,0]) }
+$Chess.each_value{|pic| SDL.SetColorKey(pic,SDL::SRCCOLORKEY,0x00FF00FF) }
 
 #以列為主的棋盤
 $board=Array.new(3){ Array.new(3){ nil } }
@@ -45,7 +45,7 @@ def click(mouse_x,mouse_y)
     $side=:O
   end
   #設定標題列提醒
-  SDL::WM.set_caption("現在輪到#{$side}下","")
+  SDL.WM_SetCaption("現在輪到#{$side}下","")
 
   #回傳獲勝方
   return winner
@@ -73,33 +73,33 @@ end
 #繪製背景
 def draw_background
   #填滿背景顏色
-  $Screen.fill_rect(0,0,300,300,[255,255,255])
+  SDL.FillRect($Screen,SDL::Rect.new([0,0,300,300]),0x00FFFFFF)
 
   #把井字畫出來
-  $Screen.draw_line(10,100,290,100,[0,0,0])
-  $Screen.draw_line(10,200,290,200,[0,0,0])
-  $Screen.draw_line(100,10,100,290,[0,0,0])
-  $Screen.draw_line(200,10,200,290,[0,0,0])
+  SDL.FillRect($Screen,SDL::Rect.new([10,100,280,1]),0)
+  SDL.FillRect($Screen,SDL::Rect.new([10,200,280,1]),0)
+  SDL.FillRect($Screen,SDL::Rect.new([100,10,1,280]),0)
+  SDL.FillRect($Screen,SDL::Rect.new([200,10,1,280]),0)
 end
 
 #重複運行直到取得結果
 winner=nil
 until(winner)
   #判斷觸發了何種事件
-  case event=SDL::Event.poll
-  when SDL::Event::KeyDown
+  case event=SDL.PollEvent
+  when SDL::KeyboardEvent
     #按下了ESC就直接結束
     case event.sym
     when SDL::Key::ESCAPE
       exit
     end
-  when SDL::Event::MouseButtonDown
+  when SDL::MouseButtonEvent
     #只判斷按下滑鼠左鍵的狀況
     case event.button
-    when SDL::Mouse::BUTTON_LEFT
+    when SDL::BUTTON_LEFT
       winner=click(event.x,event.y)
     end
-  when SDL::Event::Quit
+  when SDL::QuitEvent
     #按下了右上角的關閉就直接結束
     exit
   end
@@ -110,25 +110,27 @@ until(winner)
   $board.each_with_index{|row,y|
     row.each_with_index{|chess,x|
       #只有有棋子才需要畫
-      $Chess[chess] and $Screen.put($Chess[chess],x*100,y*100)
+      $Chess[chess] and 
+	  SDL.BlitSurface($Chess[chess],nil,$Screen,SDL::Rect.new([x*100,y*100,0,0]))
     }
   }
 
   #把畫圖的結果顯示在螢幕上
-  $Screen.flip
+  SDL.Flip($Screen)
   #讓CPU短暫休息
-  SDL.delay(16)
+  SDL.Delay(16)
 end
 
 #判斷是否單方面獲勝並在螢幕上以及標題列顯示獲勝訊息
 if winner==:X||winner==:O
-  SDL::WM.set_caption("#{winner}獲勝,遊戲結束","")
-  $Screen.put(SDL::Surface.load("#{winner}_Wins.bmp"),0,100)
+  SDL.WM_SetCaption("#{winner}獲勝,遊戲結束","")
+  SDL.BlitSurface(SDL::LoadBMP("#{winner}_Wins.bmp"),nil,$Screen,SDL::Rect.new([0,100,0,0]))
+  #$Screen.put(SDL::Surface.load(),0,100)
 else
-  SDL::WM.set_caption("平手,遊戲結束","")
-  $Screen.put(SDL::Surface.load("Draw.bmp"),0,100)
+  SDL.WM_SetCaption("平手,遊戲結束","")
+  SDL.BlitSurface(SDL::LoadBMP("Draw.bmp"),nil,$Screen,SDL::Rect.new([0,100,0,0]))
 end
 #顯示更新的畫面
-$Screen.flip
+SDL.Flip($Screen)
 #顯示5秒後自動關閉
-SDL.delay(5000)
+SDL.Delay(5000)
