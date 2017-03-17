9bd0fb6936319cad7d4318beb93426fd7450a0ab
aigecko <aigecko@hotmail.com>
2017-03-17 17:19:47 +0800

    complete slide

--- slides/20170317-RB-Gosu/main.rb
+++ slides/20170317-RB-Gosu/main.rb
@@ -8,6 +8,10 @@ class Point
   def update(x,y)
     @x,@y = x,y
   end
+  def draw(window,z)
+    window.draw_line(@x-5,@y-5,Gosu::Color::RED,@x+5,@y+5,Gosu::Color::RED,z)
+    window.draw_line(@x-5,@y+5,Gosu::Color::RED,@x+5,@y-5,Gosu::Color::RED,z)
+  end
 end
 class GameWindow < Gosu::Window
   def initialize
@@ -15,7 +19,7 @@ class GameWindow < Gosu::Window
     self.caption = "請直接輸入圖檔檔名"
     self.text_input = Gosu::TextInput.new
     @font = Gosu::Font.new(24)
-    @Zorder = { Pic: 0, Font: 1}
+    @Zorder = { Pic: 0,Point: 1, Font: 2}
     @points = [
       Point.new(200, 200), Point.new(600, 200),
       Point.new(600, 400), Point.new(200, 400)
@@ -41,7 +45,7 @@ class GameWindow < Gosu::Window
       button_down?(Gosu::MsLeft) or @drag = nil
     else
       button_down?(Gosu::MsLeft) and @drag = @points.select{|point| 
-        (point.x - mouse_x)**2 + (point.y - mouse_y)**2 < 10
+        (point.x - mouse_x)**2 + (point.y - mouse_y)**2 < 100
       }.first
     end
   end
@@ -49,6 +53,7 @@ class GameWindow < Gosu::Window
     return true
   end
   def draw
+    @points.each{|point| point.draw(self,@Zorder[:Point])}
     @pic&.draw_as_quad(@points[0].x, @points[0].y, 0xFFFFFFFF,
                        @points[1].x, @points[1].y, 0xFFFFFFFF,
                        @points[2].x, @points[2].y, 0xFFFFFFFF,
