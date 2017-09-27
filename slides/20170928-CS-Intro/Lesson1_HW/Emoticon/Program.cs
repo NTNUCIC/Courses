99d87a824272c26ef4c1ee4cda1cf756eee50a78
Kei Liao <helenhelens89227@gmail.com>
2017-09-28 00:37:34 +0800

    完成投影片#1

---
+++ slides/20170928-CS-Intro/Lesson1_HW/Emoticon/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Emoticon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("==========顏文字清單==========");
+
+            Console.WriteLine("~喜~\n");
+            Console.WriteLine("(*´ w `)~");
+            Console.WriteLine("\\(*．▽．)/");
+            Console.WriteLine("\\(●´ u `●)/");
+            Console.WriteLine("(\\\\´\\3\\`\\\\)");
+
+            Console.ReadLine();
+            Console.Clear();
+
+            Console.WriteLine("~怒~\n");
+            Console.WriteLine("(#`A´)/");
+            Console.WriteLine("<(#`皿´)>");
+            Console.WriteLine("\\(#`^´)/");
+            Console.WriteLine("(+ ˋAˊ) ");
+
+            Console.ReadLine();
+            Console.Clear();
+
+            Console.WriteLine("~哀~\n");
+            Console.WriteLine("(ˊ;ω;ˋ ) ");
+            Console.WriteLine("(╥﹏╥)");
+            Console.WriteLine("(T﹏T)");
+            Console.WriteLine("(/O n O\\)");
+
+            Console.ReadLine();
+            Console.Clear();
+
+            Console.WriteLine("~樂~\n");
+            Console.WriteLine("(*´ w `)~");
+            Console.WriteLine("\\(*．▽．)/");
+            Console.WriteLine("\\(●´ u `●)/");
+            Console.WriteLine("(\\\\´\\3\\`\\\\)");
+
+            Console.ReadLine();
+        }
+    }
+}
