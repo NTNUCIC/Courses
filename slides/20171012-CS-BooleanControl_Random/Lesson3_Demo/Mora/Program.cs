1d722c1956820ce3d452375d62bd3a8e94553cc1
Kei <kei_liao@cmoney.com.tw>
2017-10-12 11:32:46 +0800

    #3完成

---
+++ slides/20171012-CS-BooleanControl_Random/Lesson3_Demo/Mora/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rd = new Random(Guid.NewGuid().GetHashCode());
+            int yours;
+            int computers;
+
+            Console.Write("你要出(1)剪刀 (2)石頭 (3)布?");
+
+            int.TryParse(Console.ReadLine(), out yours);
+            computers = rd.Next(1, 3);
+
+            Console.WriteLine("電腦出的是: {0}", computers);
+
+            if (yours == 1)
+            {
+                if (computers == 1)
+                {
+                    Console.WriteLine("平手");
+                }
+                else if (computers == 2)
+                {
+                    Console.WriteLine("Win: 電腦");
+                }
+                else if (computers == 3)
+                {
+                    Console.WriteLine("Win: 玩家");
+                }
+                else
+                {
+                    Console.WriteLine("錯誤的輸入");
+                }
+            }
+            else if (yours == 2)
+            {
+                if (computers == 1)
+                {
+                    Console.WriteLine("Win: 玩家");
+                }
+                else if (computers == 2)
+                {
+                    Console.WriteLine("平手");
+                }
+                else if (computers == 3)
+                {
+                    Console.WriteLine("Win: 電腦");
+                }
+                else
+                {
+                    Console.WriteLine("錯誤的輸入");
+                }
+            }
+            else if (yours == 3)
+            {
+                if (computers == 1)
+                {
+                    Console.WriteLine("Win: 電腦");
+                }
+                else if (computers == 2)
+                {
+                    Console.WriteLine("Win: 玩家");
+                }
+                else if (computers == 3)
+                {
+                    Console.WriteLine("平手");
+                }
+                else
+                {
+                    Console.WriteLine("錯誤的輸入");
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ slides/20171012-CS-BooleanControl_Random/Lesson3_HW/AskGod/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AskGod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //true - 正(陽)
+            //false - 反(反)
+            bool left;
+            bool right;
+            Random rd = new Random(Guid.NewGuid().GetHashCode());
+
+            Console.WriteLine("==========擲筊==========");
+            Console.WriteLine("請按Enter鍵開始");
+            Console.ReadLine();
+
+            left = rd.Next(0, 1) == 0;
+            right = rd.Next(0, 1) == 0;
+
+            if (left == right)
+            {
+                if (left == true)
+                {
+                    Console.WriteLine("你擲出的是: 笑筊");
+                }
+                else
+                {
+                    Console.WriteLine("你擲出的是: 陰筊");
+                }
+            }
+            else
+            {
+                Console.WriteLine("你擲出的是: 聖筊");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
