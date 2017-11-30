6adec9a956d8790708838e0b43204b53b060f0c8
Kei <kei_liao@cmoney.com.tw>
2017-11-30 13:26:04 +0800

    新增#6投影片

---
+++ slides/20171130-CS-Class/Lesson6_Demo/Lesson6_Demo/MyClass.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson6_Demo
+{
+    /// <summary>
+    /// 自訂義的類別
+    /// </summary>
+    class MyClass
+    {
+        /// <summary>
+        /// 數字
+        /// Field 欄位
+        /// </summary>
+        private int myNumber;
+
+        /// <summary>
+        /// 數字
+        /// Property 屬性
+        /// </summary>
+        public int Number
+        {
+            get
+            {
+                return myNumber;
+            }
+            set
+            {
+                myNumber = value;
+            }
+        }
+
+        /// <summary>
+        /// 屬性
+        /// </summary>
+        public int Property { get; set; }
+
+        /// <summary>
+        /// 建構子
+        /// </summary>
+        public MyClass()
+        {
+
+        }
+
+        /// <summary>
+        /// 將我的數字加上一個數字
+        /// </summary>
+        /// <param name="input">你要加的數字</param>
+        public void AddNum(int input)
+        {
+            myNumber += input;
+        }
+
+        /// <summary>
+        /// 我的數字是不是零
+        /// </summary>
+        /// <returns>是否為零</returns>
+        public bool IsZero()
+        {
+            if (myNumber == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// 清空物件資料回預設值
+        /// </summary>
+        public void ClearData()
+        {
+            clear();
+        }
+
+        /// <summary>
+        /// 清空物件資料回預設值
+        /// </summary>
+        private void clear()
+        {
+            myNumber = 0;
+            Property = 0;
+        }
+    }
+}
---
+++ slides/20171130-CS-Class/Lesson6_Demo/Lesson6_Demo/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson6_Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // null 表示不存在實體
+            MyClass myObject = null;
+            Console.WriteLine("myObject: {0}", myObject);
+
+            //建立實體
+            myObject = new MyClass();
+            Console.WriteLine("myObject.IsZero: {0}", myObject.IsZero());
+            Console.WriteLine("myObject.Number: {0}", myObject.Number);
+            Console.WriteLine("myObject.Property: {0}", myObject.Property);
+            Console.WriteLine();
+
+            myObject.Number += 3;
+            Console.WriteLine("myObject.IsZero: {0}", myObject.IsZero());
+            Console.WriteLine("myObject.Number: {0}", myObject.Number);
+            Console.WriteLine("myObject.Property: {0}", myObject.Property);
+            Console.WriteLine();
+
+            myObject.Property += 2;
+            Console.WriteLine("myObject.IsZero: {0}", myObject.IsZero());
+            Console.WriteLine("myObject.Number: {0}", myObject.Number);
+            Console.WriteLine("myObject.Property: {0}", myObject.Property);
+            Console.WriteLine();
+
+            myObject.AddNum(4);
+            Console.WriteLine("myObject.IsZero: {0}", myObject.IsZero());
+            Console.WriteLine("myObject.Number: {0}", myObject.Number);
+            Console.WriteLine("myObject.Property: {0}", myObject.Property);
+            Console.WriteLine();
+
+            myObject.ClearData();
+            Console.WriteLine("myObject.IsZero: {0}", myObject.IsZero());
+            Console.WriteLine("myObject.Number: {0}", myObject.Number);
+            Console.WriteLine("myObject.Property: {0}", myObject.Property);
+            Console.WriteLine();
+
+            //private的東西外部無法取得
+            //myObject.myNumber = 3;
+            //myObject.clear();
+
+            Console.ReadLine();
+        }
+    }
+}
