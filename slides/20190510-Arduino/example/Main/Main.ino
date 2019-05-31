1a36e2bc3e0d40684c803384ab05353f25dcb9fc
lin71008 <lin71008@gmail.com>
2019-05-31 16:49:21 +0800

    20190531 release Arduino BB-8 Final Code
    Editor: vbscript055246(https://github.com/vbscript055246)

--- slides/20190510-Arduino/example/Main/Main.ino
+++ slides/20190510-Arduino/example/Main/Main.ino
@@ -1,70 +1,87 @@
+#define KEY 10
+#define RXD 11
+#define TXD 12
 #include <SoftwareSerial.h>
 #include <Wire.h>
 #include <Servo.h>
 
-// DEBUG
-bool DEBUG = true;
-// Bluetooth
-#define KEY 10
-#define RXD 11
-#define TXD 12
-#define MAX_BTCMDLEN 128 // the maximum received command length from an Android system (over the bluetooth)
-SoftwareSerial BTSerial(RXD, TXD);
 // Servo motor
 #define Servo_1_Data 5
 #define Servo_2_Data 6
 Servo Servo_1, Servo_2;
 int pos_1 = 0, pos_2 = 0;
 
-void setup()
-{
-  // Bluetooth
-  pinMode(KEY, OUTPUT);
-  digitalWrite(KEY, LOW); // AT Mode -> Normal(?) Mode
+
+SoftwareSerial BT(RXD, TXD);
+
+void setup() {
+  BT.begin(38400);
   Serial.begin(9600);
-  BTSerial.begin(38400);
-  // Servo motor
+  
+  //setting motor
+  pinMode(KEY, OUTPUT);
+  digitalWrite(KEY, LOW);
+  
+  //testing motor
   Servo_1.attach(Servo_1_Data,500,2400);
   Servo_2.attach(Servo_2_Data,500,2400);
   Servo_1.write(90);
   Servo_2.write(90);
-  delay(3000); // Wait for servo motor moving
+  delay(3000);
 }
 
-void loop()
-{
-  // Bluetooth
-  BTSerial.listen();
-  char cmd[MAX_BTCMDLEN];
-  int len = 0;
-  memset(cmd,0,MAX_BTCMDLEN);
-  for(int i = 0; i < MAX_BTCMDLEN; i++)
-  {
-    for(int _len = 0; _len < BTSerial.available(); _len++)
-    {
-      cmd[(len++)%MAX_BTCMDLEN] = char(BTSerial.read());
+void loop() {
+  int cmd[3],ptr=0;
+  int table[5][3]={
+      {128,128,128}, //2
+      {248,128,128}, //3
+      {120,248,128}, //5
+      {128,248,128}, //6
+      {248,248,128}  //7
+    };
+  while(1){
+    while(BT.available()>0){;
+      cmd[ptr] = BT.read();
+      ptr++;
     }
-  }
-  // Servo motor
-  // Main Function ?
-  if(len != 0)
-  {
-    if(cmd == "0")  { pos_1 =   0;}
-    if(cmd == "1")  { pos_1 =  90;}
-    if(cmd == "2")  { pos_1 = -90;}
-    if(cmd == "3")  { pos_2 =   0;}
-    if(cmd == "4")  { pos_2 =  90;}
-    if(cmd == "5") { pos_2 =  -90;}
-    if(DEBUG)
-    {
-      Serial.print("Command: ");Serial.print(cmd);
-      Serial.print(", Pos: ");Serial.print(pos_1+90);
-      Serial.print(", ");Serial.println(pos_2+90);
+    if(ptr == 3){
+      for(int i=0;i<5;i++){
+        int flag = 1;
+        for(int j=0;j<3;  .j++){
+          if(table[i][j]!=cmd[j]){
+            flag = 0;
+            break;
+          }
+        }
+        if(flag > 0){
+          if(i == 0){ //set to 180
+            Servo_1.write(180);
+            Serial.println(2);
+          }
+          else if(i == 1){ //set to 0
+            Servo_1.write(0);
+            Serial.println(3);
+          }
+          else if(i == 2){ //set to 180
+            Servo_2.write(180);
+            Serial.println(5);
+          }
+          else if(i == 3){ //set to 0
+            Servo_2.write(0);
+            Serial.println(6);
+          }
+          else if(i == 4){ //set both to 90
+            Servo_2.write(90);
+            Servo_1.write(90);
+            Serial.println(7);
+          }
+          else{
+            Serial.println("ERROR");
+          }
+        }
+      }
+      ptr = 0;
     }
-    Servo_1.write(90 + pos_1);
-    Servo_2.write(90 + pos_2);
-    delay(100);
     
   }
-
 }
